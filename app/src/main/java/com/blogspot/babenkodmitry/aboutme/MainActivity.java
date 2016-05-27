92a3c98f672e7a75f2683a514a5d9f76f52adc88
dmitrybabenko <mail2babenko@gmail.com>
2016-05-27 11:47:45 +0300

    Lowered minimum supported SDK version to 15

--- app/src/main/java/com/blogspot/babenkodmitry/aboutme/MainActivity.java
+++ app/src/main/java/com/blogspot/babenkodmitry/aboutme/MainActivity.java
@@ -1,11 +1,11 @@
 package com.blogspot.babenkodmitry.aboutme;
 
 import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -32,19 +32,19 @@ public class MainActivity extends AppCompatActivity {
 
         result.add(new CardDataObject(getString(R.string.it_skills),
                 arrayToString(getResources().getStringArray(R.array.it_skills)),
-                getDrawable(R.drawable.ic_work_black_24dp)));
+                ContextCompat.getDrawable(this, R.drawable.ic_work_black_24dp)));
 
         result.add(new CardDataObject(getString(R.string.education),
                 arrayToString(getResources().getStringArray(R.array.education)),
-                getDrawable(R.drawable.ic_school_black_24dp)));
+                ContextCompat.getDrawable(this, R.drawable.ic_school_black_24dp)));
 
         result.add(new CardDataObject(getString(R.string.languages),
                 arrayToString(getResources().getStringArray(R.array.languages)),
-                getDrawable(R.drawable.ic_translate_black_24dp)));
+                ContextCompat.getDrawable(this, R.drawable.ic_translate_black_24dp)));
 
         result.add(new CardDataObject(getString(R.string.interests),
                 arrayToString(getResources().getStringArray(R.array.interests)),
-                getDrawable(R.drawable.ic_favorite_black_24dp)));
+                ContextCompat.getDrawable(this, R.drawable.ic_favorite_black_24dp)));
         return result;
     }
 
